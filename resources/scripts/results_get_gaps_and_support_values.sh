ce43b4fc6154a00e0d2b0b812a475c277e73e152
Hugo López-Fernández <hlfernandez@uvigo.es>
2020-10-18 10:06:50 +0200

    Use /tmp and remove duplicated positions in output

--- resources/scripts/results_get_gaps_and_support_values.sh
+++ resources/scripts/results_get_gaps_and_support_values.sh
@@ -7,14 +7,13 @@ HOST_WORKING_DIR=$3
 FILENAME_WITHOUT_EXT="${ORIGINAL_FILENAME%.*}"
 FILENAME=${FILENAME_WITHOUT_EXT}
 
-TMP_DIR=${WORKING_DIR}/tmp/${FILENAME}
-mkdir -p ${TMP_DIR}
+TMP_DIR=$(mktemp -d /tmp/support-values-${FILENAME}.XXXX)
 
 #Get alignment gaps
 
-docker run --rm -v ${HOST_WORKING_DIR}:/data pegi3s/alter -i /data/input/6_aligned/${FILENAME}.aln -o /data/tmp/${FILENAME}/${FILENAME}.fasta -ia -of FASTA -oo Linux -op GENERAL
+docker run --rm -v ${HOST_WORKING_DIR}/:/data -v /tmp:/tmp pegi3s/alter -i /data/input/6_aligned/${FILENAME}.aln -o ${TMP_DIR}/${FILENAME}.fasta -ia -of FASTA -oo Linux -op GENERAL
 
-docker run --rm -v ${HOST_WORKING_DIR}/:/data pegi3s/utilities fasta_remove_line_breaks /data/tmp/${FILENAME}/${FILENAME}.fasta
+docker run --rm -v /tmp:/tmp pegi3s/utilities fasta_remove_line_breaks ${TMP_DIR}/${FILENAME}.fasta
 
 grep -v '>' ${TMP_DIR}/${FILENAME}.fasta > ${TMP_DIR}/${FILENAME}.fasta.tmp1
 
@@ -32,14 +31,16 @@ do
 	echo $corrvalue >> ${TMP_DIR}/${FILENAME}.fasta.tmp3
 done < ${TMP_DIR}/${FILENAME}.fasta.tmp2
 
+sort -u ${TMP_DIR}/${FILENAME}.fasta.tmp3 > ${TMP_DIR}/${FILENAME}.fasta.tmp4
+
 echo "Gapped sites in protein alignment" >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
-echo $(cat ${TMP_DIR}/${FILENAME}.fasta.tmp3) >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
+echo $(cat ${TMP_DIR}/${FILENAME}.fasta.tmp4) >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
 
 #Get low support values
 
-docker run --rm -v ${HOST_WORKING_DIR}:/data pegi3s/alter -i /data/input/10_gapped_alignment/2_gapped_alignment/${FILENAME}.aln -o /data/tmp/${FILENAME}/${FILENAME}.fasta.support -ia -of FASTA -oo Linux -op GENERAL
+docker run --rm -v ${HOST_WORKING_DIR}/:/data -v /tmp:/tmp pegi3s/alter -i /data/input/10_gapped_alignment/2_gapped_alignment/${FILENAME}.aln -o ${TMP_DIR}/${FILENAME}.fasta.support -ia -of FASTA -oo Linux -op GENERAL
 
-docker run --rm -v ${HOST_WORKING_DIR}/:/data pegi3s/utilities fasta_remove_line_breaks /data/tmp/${FILENAME}/${FILENAME}.fasta.support
+docker run --rm -v /tmp:/tmp pegi3s/utilities fasta_remove_line_breaks ${TMP_DIR}/${FILENAME}.fasta.support
 
 grep -v '>' ${TMP_DIR}/${FILENAME}.fasta.support > ${TMP_DIR}/${FILENAME}.fasta.tmp1.support
 
@@ -57,6 +58,8 @@ do
 	echo $corrvalue >> ${TMP_DIR}/${FILENAME}.fasta.tmp3.support
 done < ${TMP_DIR}/${FILENAME}.fasta.tmp2.support
 
+touch ${TMP_DIR}/${FILENAME}.fasta.tmp4.support
+
 while read line
 do
 	if grep -q "\b$line\b" ${TMP_DIR}/${FILENAME}.fasta.tmp3; then
@@ -66,9 +69,10 @@ do
 	fi
 done < ${TMP_DIR}/${FILENAME}.fasta.tmp3.support
 
-touch ${TMP_DIR}/${FILENAME}.fasta.tmp4.support
+sort -u ${TMP_DIR}/${FILENAME}.fasta.tmp4.support > ${TMP_DIR}/${FILENAME}.fasta.tmp5.support
+
 echo "Low support positions in the alignment" >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
-echo $(cat ${TMP_DIR}/${FILENAME}.fasta.tmp4.support) >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
+echo $(cat ${TMP_DIR}/${FILENAME}.fasta.tmp5.support) >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
 
 recomb=$(cat ${WORKING_DIR}/input/results/master_alignment/phipack/${ORIGINAL_FILENAME}/phipack.log.summary)
 echo "Evidence for recombination in the master alignment" >> ${WORKING_DIR}/results/tabulated/${ORIGINAL_FILENAME}
